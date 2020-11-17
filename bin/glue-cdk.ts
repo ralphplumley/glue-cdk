887214e6a03c8b9a28bab22dbfcc0f6ff6630a36
Ralph Plumley <ralphplumley@gmail.com>
2020-11-17 14:09:34 -0500

    Initial commit

---
+++ bin/glue-cdk.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { GlueCdkStack } from '../lib/glue-cdk-stack';
+
+const app = new cdk.App();
+new GlueCdkStack(app, 'GlueCdkStack');
